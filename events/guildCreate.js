7ceee98344b96dd3c2110e3b48ac276104dd8aad
whopper-z <79403251+whopper-z@users.noreply.github.com>
2021-06-03 05:33:07 +0300

    Add files via upload

---
+++ events/guildCreate.js
@@ -0,0 +1,16 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+const ayarlar = require('../ayarlar.json');
+
+var prefix = ayarlar.prefix;
+
+const girismesaj = [
+  'Alchera#0001',
+  'Kayıt Sistemi'
+]
+
+client.on('guildCreate', guild => {
+    const generalChannel = guild.defaultChannel
+    generalChannel.sendMessage(girismesaj)
+	client.user.setGame(prefix + 'yardım | ' + client.guilds.size + ' sunucu | ' + client.guilds.reduce((a, b) => a + b.memberCount, 0).toLocaleString() + ' kullanıcı');
+})
---
+++ komutlar/kayıtkız.js
@@ -0,0 +1,34 @@
+const Discord = require("discord.js");
+
+exports.run = async (client, message, args) => {
+  if (!message.member.hasPermission("MANAGE_NICKNAMES"))
+    return message.channel.send(
+      `❌ Bu Komutu Kullanabilmek için \`İsimleri Yönet\` Yetkisine Sahip Olmalısın!`
+    );
+  let member = message.mentions.members.first();
+  let isim = args.slice(1).join(" ");
+  let yas = args.slice(1).join(" ");
+  if (!member) return message.channel.send(":x: Bir Üye Etiketlemelisin!");
+  if (!isim) return message.channel.send(":x: Bir İsim Yazmalısın!");
+  member.setNickname(`${isim}`);
+  member.removeRole('ALINACAK ROL')
+  member.addRole('VERILECEK ROL')
+const embed = new Discord.RichEmbed()
+
+
+      .addField(`**🏷 ArdaDemr Kayıt 🏷**`,
+      `\n**🔸️Kayıt Edilen Kullanıcı:** ${member.user} \n🔸️**Kayıt Eden Yetkili:** \`${message.author.username}\``)
+client.channels.get('LOG KANAL ID').send(embed)
+};
+
+exports.conf = {
+  enabled: true,
+  guildOnly: true,
+  aliases: ["nick", "isim"],
+  permLevel: 0
+};
+exports.help = {
+  name: "kız",
+  description: "ArdaDemr Kayıt Sistemi",
+  usage: "ArdaDemr Kayıt Sistemi"
+};
